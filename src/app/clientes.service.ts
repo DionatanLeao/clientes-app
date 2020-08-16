2d87c5c6128a4966f60a643af23f2eec40e87d38
DionatanLeao <dionatan.ti@outlook.com>
2020-08-16 19:27:07 -0300

    Módulo HTTP Angular

--- src/app/clientes.service.ts
+++ src/app/clientes.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 import { Cliente } from './clientes/clientes';
+import { HttpClient } from '@angular/common/http'
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClientesService {
 
-  constructor() { }
+  constructor( private http : HttpClient) { }
 
   getClientes() : Cliente {
     let cliente : Cliente = new Cliente();
