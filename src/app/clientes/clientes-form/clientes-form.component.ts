203af6cbffb2035fb1bd1d8d55cf8df498afcfac
DionatanLeao <dionatan.ti@outlook.com>
2020-10-12 22:11:27 -0300

    Trabalhando com rotas filhas e templates diferentes

--- src/app/clientes/clientes-form/clientes-form.component.ts
+++ src/app/clientes/clientes-form/clientes-form.component.ts
@@ -68,7 +68,7 @@ export class ClientesFormComponent implements OnInit {
   }
 
   voltarParaLista() {
-    this.router.navigate(['/clientes-lista'])
+    this.router.navigate(['/clientes/lista'])
   }
 
 }
