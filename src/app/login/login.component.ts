9398b94c6851ef3619c6fb60d3e1156f9d1306d6
DionatanLeao <dionatan.ti@outlook.com>
2020-10-17 13:20:14 -0300

    Finalizando o cadastro de Usuário

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -12,9 +12,9 @@ export class LoginComponent {
 
     username: string;
     password: string;
-    loginError: boolean;
     cadastrando: boolean;
     mensagemSucesso: string;
+    errors: String[];
 
     constructor(
       private router: Router,
@@ -42,10 +42,9 @@ export class LoginComponent {
         .salvar(usuario)
         .subscribe( response => {
             this.mensagemSucesso = "Cadastro relaizado com sucesso! Efetue o login."; 
-            this.loginError = false;
-        }, error => {
-          this.loginError = true;
+        }, errorResponse => {
           this.mensagemSucesso = null;
+          this.errors = errorResponse.error.errors;
         })
     }
 
