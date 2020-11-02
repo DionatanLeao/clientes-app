fc40c6ea98e76bd9ea50cda9e9fd8db2be6c1426
DionatanLeao <dionatan.ti@outlook.com>
2020-11-02 13:02:32 -0300

    Controlando a Sessão de Usuário com o Token

--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
 import { AuthService } from './auth.service';
 
 @Injectable({
@@ -8,7 +8,8 @@ import { AuthService } from './auth.service';
 export class AuthGuard implements CanActivate {
   
   constructor(
-    private authService : AuthService
+    private authService : AuthService,
+    private router : Router
   ) {
 
   }
@@ -17,7 +18,15 @@ export class AuthGuard implements CanActivate {
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): boolean {
       
-    return this.authService.isAuthenticated();
+    const authenticated = this.authService.isAuthenticated();
+
+    if(authenticated) {
+      return true;
+    }
+    else {
+      this.router.navigate(['/login'])
+      return false;
+    }
   }
   
 }
